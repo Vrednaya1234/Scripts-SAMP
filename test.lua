828bb13a3f73d6b4a1c2ba5a33f545fb7e1c5e69
Vrednaya1234 <82057436+Vrednaya1234@users.noreply.github.com>
2021-06-15 01:52:56 +0300

    Add files via upload

---
+++ test.lua
@@ -0,0 +1,68 @@
+local imgui = require 'imgui'
+local key = require 'vkeys'
+-- вот это надо
+script_version("2.0")
+local color = 0x348cb2
+-- это тоже
+local dlstatus = require('moonloader').download_status
+
+local main_window_state = imgui.ImBool(false)
+function imgui.OnDrawFrame()
+  if main_window_state.v then
+    imgui.SetNextWindowSize(imgui.ImVec2(150, 200), imgui.Cond.FirstUseEver)
+    imgui.Begin('Testing update 2/0', main_window_state)
+    if imgui.Button('UPDATE ME!') then
+      printStringNow('Updating!', 1000)
+      update()
+    end
+    imgui.Text("Now version "..thisScript().version)
+    imgui.End()
+  end
+end
+
+function update()
+  local fpath = os.getenv('TEMP') .. '\\testing_version.json' -- куда будет качаться наш файлик для сравнения версии
+  downloadUrlToFile('https://raw.githubusercontent.com/Vrednaya1234/Scripts-SAMP/main/update.json', fpath, function(id, status, p1, p2) -- ссылку на ваш гитхаб где есть строчки которые я ввёл в теме или любой другой сайт
+    if status == dlstatus.STATUS_ENDDOWNLOADDATA then
+    local f = io.open(fpath, 'r') -- открывает файл
+    if f then
+      local info = decodeJson(f:read('*a')) -- читает
+      updatelink = info.updateurl
+      if info and info.latest then
+        version = tonumber(info.latest) -- переводит версию в число
+        if version > tonumber(thisScript().version) then -- если версия больше чем версия установленная то...
+          lua_thread.create(goupdate) -- апдейт
+        else -- если меньше, то
+          update = false -- не даём обновиться 
+          sampAddChatMessage(('[Testing]: У вас и так последняя версия! Обновление отменено'), color)
+        end
+      end
+    end
+  end
+end)
+end
+--скачивание актуальной версии
+function goupdate()
+sampAddChatMessage(('[Testing]: Обнаружено обновление. AutoReload может конфликтовать. Обновляюсь...'), color)
+sampAddChatMessage(('[Testing]: Текущая версия: '..thisScript().version..". Новая версия: "..version), color)
+wait(300)
+downloadUrlToFile(updatelink, thisScript().path, function(id3, status1, p13, p23) -- качает ваш файлик с latest version
+  if status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
+  sampAddChatMessage(('[Testing]: Обновление завершено!'), color)
+  thisScript():reload()
+end
+end)
+end
+
+-- ВСЁ!
+
+function main()
+sampAddChatMessage(("ЭТО ОБНОРВА"), color)
+  while true do
+    wait(0)
+    if wasKeyPressed(key.VK_X) then -- активация по нажатию клавиши X
+        main_window_state.v = not main_window_state.v -- переключаем статус активности окна, не забываем про .v
+    end
+    imgui.Process = main_window_state.v -- теперь значение imgui.Process всегда будет задаваться в зависимости от активности основного окна
+  end
+end
